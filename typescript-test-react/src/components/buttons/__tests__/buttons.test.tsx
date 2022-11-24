f844f7f4a824db1e80cff3be0d7764b9cb1bc144
Felix Tjernberg <felix.tjernberg@hotmail.se>
2022-11-24 11:18:17 +0100

    Refactor button test to AAA and add click scenario

--- typescript-test-react/src/components/buttons/__tests__/buttons.test.tsx
+++ typescript-test-react/src/components/buttons/__tests__/buttons.test.tsx
@@ -5,20 +5,41 @@ import { SubmitButton } from '../submit/submitButton'
 
 describe('Button tests', () => {
   test('Testing submit button', () => {
+    // Arrange
+    const color = 'red'
+    const border = '1px solid black'
+    const radius = '5px'
+    const height = '50px'
+    const width = '100px'
+    const fontSize = '20px'
+    const onClick = () => console.log('clicked')
+    const logSpy = jest.spyOn(console, 'log')
+
+    // Act
     render(
       <SubmitButton
-        color="red"
-        border="1px solid black"
-        radius="5px"
-        height="50px"
-        width="100px"
-        fontSize="20px"
-        onClick={() => {
-          console.log('clicked')
-        }}
+        color={color}
+        border={border}
+        radius={radius}
+        height={height}
+        width={width}
+        fontSize={fontSize}
+        onClick={onClick}
       />
     )
-    expect(screen.getByTestId('submit-button')).toBeInTheDocument()
-    expect(screen.getByTestId('submit-button')).toBeEnabled()
+    const buttonOnScreen = screen.getByTestId('submit-button')
+    buttonOnScreen.click()
+
+    // Assert
+    expect(logSpy).toHaveBeenCalledWith('clicked')
+    expect(buttonOnScreen).toBeEnabled()
+    expect(buttonOnScreen).toHaveStyle({
+      backgroundColor: color,
+      border: border,
+      borderRadius: radius,
+      height: height,
+      width: width,
+      fontSize: fontSize
+    })
   })
 })
