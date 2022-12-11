5b47962d3efbbe05f05e4e44cfbdb3ff6cdbd77d
Felix Tjernberg <felix.tjernberg@hotmail.se>
2022-12-11 18:00:49 +0100

    Add border/stroke content

---
+++ typescript-test-svelte/src/stories/DesignSystemPage/Border.svelte
@@ -0,0 +1,20 @@
+<div class="flex-column justify-center gap-100">
+  {#each [100, 200, 300, 400, 500] as weight}
+    <div class="flex">
+      <p>{weight}</p>
+      <div
+        style={`border-top: var(--stroke-${weight}) solid var(--color-300);`}
+      />
+    </div>
+  {/each}
+</div>
+
+<style>
+  div.flex > div {
+    margin: auto;
+    width: 100px;
+  }
+  p {
+    margin-right: 1em;
+  }
+</style>
