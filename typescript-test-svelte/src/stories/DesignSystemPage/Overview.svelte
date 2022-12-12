40c4fdc8a4e966eb6a973fbaeecb1de67fc84f16
Felix Tjernberg <felix.tjernberg@hotmail.se>
2022-12-12 16:02:56 +0100

    Add spacing content

--- typescript-test-svelte/src/stories/DesignSystemPage/Overview.svelte
+++ typescript-test-svelte/src/stories/DesignSystemPage/Overview.svelte
@@ -3,11 +3,13 @@
   import Border from './Border.svelte'
   import Colors from './Colors.svelte'
   import Typography from './Typography.svelte'
+  import Spacing from './Spacing.svelte'
 </script>
 
 <div class="flex-column justify-center items-center gap-300">
   <Colors overview={true} />
   <Typography overview={true} />
   <Border overview={true} />
+  <Spacing />
   <Icons />
 </div>
--- typescript-test-svelte/src/stories/DesignSystemPage/Spacing.svelte
+++ typescript-test-svelte/src/stories/DesignSystemPage/Spacing.svelte
@@ -1,1 +1,19 @@
-<p>s</p>
+{#each [200, 300, 400, 500, 600, 700] as weight}
+  <div
+    class="flex justify-center items-center width-fit-content"
+    style={`padding: var(--spacing-root-${weight}); margin: auto; margin-top: 1em`}
+  >
+    <div class="flex justify-center items-center">{weight}</div>
+  </div>
+{/each}
+
+<style>
+  div {
+    background-color: var(--color-300);
+    border-radius: var(--border-radius-100);
+  }
+  div > div {
+    background-color: var(--color-100);
+    padding: 1em;
+  }
+</style>
